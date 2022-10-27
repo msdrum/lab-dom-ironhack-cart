56accbfa02ba3562de72654fb13ce447a4f9d79f
Marcio Silva <marciosilvadrum@gmail.com>
2022-10-26 21:00:23 -0300

    interation 1

--- js/index.js
+++ js/index.js
@@ -2,20 +2,21 @@
 
 function updateSubtotal(product) {
   console.log('Calculating subtotal, yey!');
-
-  //... your code goes here
+  const price = parseInt(product.querySelector('.price span').textContent);
+  const quantity = parseInt(product.querySelector('.quantity input').value);
+  const subTotalValue = price * quantity;
+  const subTotal = product.querySelector('.subtotal span');
+  subTotal.innerHTML = subTotalValue;
+  return subTotalValue;
 }
 
 function calculateAll() {
   // code in the following two lines is added just for testing purposes.
   // it runs when only iteration 1 is completed. at later point, it can be removed.
-  const singleProduct = document.querySelector('.product');
-  updateSubtotal(singleProduct);
+  // const singleProduct = document.querySelector('.product');
+  // updateSubtotal(singleProduct);
   // end of test
-
   // ITERATION 2
-  //... your code goes here
-
   // ITERATION 3
   //... your code goes here
 }
@@ -35,7 +36,9 @@ function createProduct() {
 }
 
 window.addEventListener('load', () => {
+  //botão calculate prices
   const calculatePricesBtn = document.getElementById('calculate');
+  //colocar o evento de click
   calculatePricesBtn.addEventListener('click', calculateAll);
 
   //... your code goes here
